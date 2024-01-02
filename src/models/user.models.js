c80ad652af81eed91d29eea8818cfbebb83123b0
Prince Kumar <princepk7878kumar@gmail.com>
2024-01-02 09:18:07 +0530

    done User and video model

---
+++ src/models/user.models.js
@@ -0,0 +1,56 @@
+import mongoose,{Schema} from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+
+const userSchema=new Schema({
+       username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+       },
+       email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+       },
+       fullName:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+       },
+       avatar:{
+        type:String,   //cloundinary url
+        required:true
+       },
+       coverImage:{
+        type:String,
+       },
+       watchHistory:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        }
+       ],
+       password:{
+        type:String,
+        required:[true,"Password is required"]
+       },
+       refreshToken:{
+         type:String,
+       },
+      
+       
+},{timestamps:true})
+
+
+
+
+
+export const User=mongoose.model("User",userSchema)
