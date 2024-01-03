4259cffa15370b34f35ba7011169211717613306
Prince Kumar <princepk7878kumar@gmail.com>
2024-01-03 20:10:05 +0530

    added logout method

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken"
 import { User } from "../models/user.models";
 
 
-export const verifyJWT = asyncHandler(async (req,res,next)=>{
+export const verifyJWT = asyncHandler(async (req, _,next)=>{
    try {
      const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
  
